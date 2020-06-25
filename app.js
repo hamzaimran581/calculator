4a820a39a4e48003933ace9ceabd545600b10ba3
hamzaimran581 <hamzaimran581@gmail.com>
2020-06-26 00:23:02 +0500

    calculator using html and js

---
+++ app.js
@@ -0,0 +1,13 @@
+function  displayHere(number){
+    var here = document.getElementById("show");
+    here.value += number;
+}
+function  pak(){
+    var here = document.getElementById("show");
+    here.value = "";
+}
+
+function gettingThings(){
+    var here = document.getElementById("show");
+    show.value = eval(show.value)
+}
